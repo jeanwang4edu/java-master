64ff1c68d1ed76bbd9fa8dc67185b5ea07e52d7e
jeanwang4edu <ywang24@wccnet.edu>
2020-09-23 15:56:32 -0400

    add coding exe 30 Person

--- CodingExe/src/study/jeanwang2dev/Main.java
+++ CodingExe/src/study/jeanwang2dev/Main.java
@@ -6,15 +6,29 @@ public class Main {
 
     public static void main(String[] args) {
 
-        SimpleCalculator calculator= new SimpleCalculator();
-        calculator.setFirstNumber(5.0);
-        calculator.setSecondNumber(4);
-        System.out.println("add= " + calculator.getAdditionResult());
-        System.out.println("subtract= " + calculator.getSubtractionResult());
-        calculator.setFirstNumber(5.25);
-        calculator.setSecondNumber(0);
-        System.out.println("multiply= " + calculator.getMultiplicationResult());
-        System.out.println("divde=" + calculator.getDivisionResult());
+          Person person = new Person();
+          person.setFirstName("");
+          person.setLastName("");
+          person.setAge(10);
+          System.out.println("fullName= " + person.getFullName());
+          System.out.println("teen= " + person.isTeen());
+          person.setFirstName("John");
+          person.setAge(18);
+          System.out.println("fullName= " + person.getFullName());
+          System.out.println("teen= " + person.isTeen());
+          person.setLastName("Smith");
+          System.out.println("fullName= " + person.getFullName());
+
+
+//        SimpleCalculator calculator= new SimpleCalculator();
+//        calculator.setFirstNumber(5.0);
+//        calculator.setSecondNumber(4);
+//        System.out.println("add= " + calculator.getAdditionResult());
+//        System.out.println("subtract= " + calculator.getSubtractionResult());
+//        calculator.setFirstNumber(5.25);
+//        calculator.setSecondNumber(0);
+//        System.out.println("multiply= " + calculator.getMultiplicationResult());
+//        System.out.println("divde=" + calculator.getDivisionResult());
 //        System.out.println(AreaCalculator.area(5.0));
 //        System.out.println(AreaCalculator.area(-1));
 //        System.out.println(AreaCalculator.area(5.0, 4.0));
