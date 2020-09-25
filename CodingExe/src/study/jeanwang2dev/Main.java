6de10b9bda67333e7e9652f4a5e97181fcd3684d
jeanwang4edu <ywang24@wccnet.edu>
2020-09-25 13:17:26 -0400

    add coding exe 34 Complex Operations

--- CodingExe/src/study/jeanwang2dev/Main.java
+++ CodingExe/src/study/jeanwang2dev/Main.java
@@ -6,13 +6,24 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Point first = new Point(6,5);
-        Point second = new Point(3,1);
-        System.out.println("distance(0,0)= " + first.distance() );
-        System.out.println("distance(second)= " + first.distance(second));
-        System.out.println("distance(2,2)= " + first.distance(2,2));
-        Point point = new Point();
-        System.out.println("distance()= " + point.distance());
+        ComplexNumber one = new ComplexNumber(1.0, 1.0);
+        ComplexNumber number = new ComplexNumber(2.5, -1.5);
+        one.add(1,1);
+        System.out.println("one.real= " + one.getReal());
+        System.out.println("one.imaginary= " + one.getImaginary());
+        one.subtract(number);
+        System.out.println("one.real= " + one.getReal());
+        System.out.println("one.imaginary= " + one.getImaginary());
+        number.subtract(one);
+        System.out.println("number.real= " + number.getReal());
+        System.out.println("number.imaginary= " + number.getImaginary());
+//        Point first = new Point(6,5);
+//        Point second = new Point(3,1);
+//        System.out.println("distance(0,0)= " + first.distance() );
+//        System.out.println("distance(second)= " + first.distance(second));
+//        System.out.println("distance(2,2)= " + first.distance(2,2));
+//        Point point = new Point();
+//        System.out.println("distance()= " + point.distance());
 //          Wall wall = new Wall(1.125,-1.0);
 //          System.out.println("area= " + wall.getArea());
 //
