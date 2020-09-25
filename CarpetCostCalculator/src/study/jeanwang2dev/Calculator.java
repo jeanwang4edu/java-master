263861fbb927a4237f4258499f1717ae1d54ba58
jeanwang4edu <ywang24@wccnet.edu>
2020-09-25 10:59:20 -0400

    Add coding exe 33 Carpet Cost Calculator more

---
+++ CarpetCostCalculator/src/study/jeanwang2dev/Calculator.java
@@ -0,0 +1,17 @@
+package study.jeanwang2dev;
+
+public class Calculator {
+
+    private Floor floor;
+    private Carpet carpet;
+
+    public Calculator(Floor floor, Carpet carpet) {
+        this.floor = floor;
+        this.carpet = carpet;
+    }
+
+    public double getTotalCost(){
+        return floor.getArea() * carpet.getCost();
+    }
+
+}
