7b3f2cbe1ce24f18a5ab6c10afe3a9bcce12f0f1
jeanwang4edu <ywang24@wccnet.edu>
2020-12-07 14:57:54 -0500

    Packages Part 1

---
+++ WinDemo/src/com/example/awtSample/Main.java
@@ -0,0 +1,12 @@
+package com.example.awtSample;
+
+/**
+ * Created by jean on 12/7/20.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        MyWindow myWindow = new MyWindow("Complete Java");
+        myWindow.setVisible(true);
+    }
+}
