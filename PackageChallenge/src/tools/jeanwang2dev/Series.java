f12fcc3ec6b2c851c4f5a1a6cb37662551d1d21d
jeanwang4edu <ywang24@wccnet.edu>
2020-12-08 09:48:09 -0500

    Modify PackageChallenge Package JAR file

--- PackageChallenge/src/tools/jeanwang2dev/Series.java
+++ PackageChallenge/src/tools/jeanwang2dev/Series.java
@@ -5,31 +5,35 @@ package tools.jeanwang2dev;
  */
 public class Series {
 
-    public int nSum(int n) {
-        int i = 0, sum = 0;
-        while( i <= n) {
-            sum += i;
-            i++;
-        }
-        return sum;
+    public static long nSum(int n) {
+//        int i = 0, sum = 0;
+//        while( i <= n) {
+//            sum += i;
+//            i++;
+//        }
+//        return sum;
+        return ( n * ( n + 1 ) ) / 2;
     }
 
-    public long fibonacci(int n) {
+    public static long fibonacci(int n) {
         if( n == 0 ) return 0;
         if( n == 1 ) return 1;
         // when n is bigger than or equal to 2
-        int num0  = 0, num1 = 1, counter = 2, num2 = 0;
+        long nMinus1 = 1, nMinus2 = 0, fib = 0;
+        int counter = 0;
         while( counter <= n ){
-            num2 = num0 + num1;
-            num0 = num1;
-            num1 = num2;
+            fib = nMinus1 + nMinus2;
+            nMinus2 = nMinus1;
+            nMinus1 = fib;
             counter++;
         }
-        return num2;
+        return fib;
     }
 
-    public long factorial(int n) {
-        int i = 1, result = 1;
+    public static   long factorial(int n) {
+        if( n == 0 ) return 1;
+        int i = 1;
+        long result = 1;
         while( i <= n) {
             result *= i;
             i++;
