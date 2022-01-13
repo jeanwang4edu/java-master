4bfe08b772176341c1c6a2e19a413ead351f9f8e
jeanwang4edu <ywang24@wccnet.edu>
2022-01-13 16:14:55 -0500

    update helloworld project

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello Jean!");
+        System.out.println("Hello Jean! test");
 
         int myFirstNumber = (10 + 5) + (2 * 10);
         int mySecondNumber = 12;
